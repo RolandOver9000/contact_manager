fe4ca98166eb8fef591e499ef73d37f89ecf2f44
Roland Csosz <roland@iot-inspector.com>
2021-09-21 18:38:30 +0200

    Created a controller for contacts

---
+++ src/main/java/hu/futureofmedia/task/contactsapi/controller/ContactPersonController.java
@@ -0,0 +1,12 @@
+package hu.futureofmedia.task.contactsapi.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/contacts")
+@CrossOrigin(origins = "*")
+public class ContactPersonController {
+    
+}
