6346856d792ee2adf978ce232f73392bda5fd29a
Roland Csosz <roland@iot-inspector.com>
2021-09-21 19:19:13 +0200

    Added the required fields for outgoing contact

--- src/main/java/hu/futureofmedia/task/contactsapi/model/dto/OutgoingContactPerson.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/model/dto/OutgoingContactPerson.java
@@ -1,4 +1,9 @@
 package hu.futureofmedia.task.contactsapi.model.dto;
 
 public class OutgoingContactPerson {
+
+    private String fullName;
+    private String companyName;
+    private String email;
+    private String phoneNumber;
 }
