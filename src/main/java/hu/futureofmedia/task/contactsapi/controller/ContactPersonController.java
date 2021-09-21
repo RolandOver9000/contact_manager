f38882925dc0e3d66e333b1b7223af5dba5cfdeb
Roland Csosz <roland@iot-inspector.com>
2021-09-21 19:07:15 +0200

    Created an endpoint for getting limited active contacts

--- src/main/java/hu/futureofmedia/task/contactsapi/controller/ContactPersonController.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/controller/ContactPersonController.java
@@ -1,12 +1,24 @@
 package hu.futureofmedia.task.contactsapi.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/contacts")
 @CrossOrigin(origins = "*")
 public class ContactPersonController {
-    
+
+    @GetMapping("/active")
+    public ResponseEntity<List<OutgoingContactPerson>> getAllActiveContactPersonsFromTo(@RequestParam("from") int from,
+                                                                                        @RequestParam("to") int to) {
+        try {
+            return ResponseEntity.ok(contactPersonService.getAllActiveContactPersonFromTo(from, to));
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR, "Error during getting active contacts from " + from + " - " + to);
+        }
+    }
+
 }
