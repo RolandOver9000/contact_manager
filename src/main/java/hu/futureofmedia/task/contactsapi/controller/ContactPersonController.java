8e415758c684f0c723d697df506030ba142d28db
Roland Csosz <roland@iot-inspector.com>
2021-09-22 15:25:38 +0200

    Refactored getAllActiveContactPersonsByPage endpoint

--- src/main/java/hu/futureofmedia/task/contactsapi/controller/ContactPersonController.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/controller/ContactPersonController.java
@@ -1,23 +1,31 @@
 package hu.futureofmedia.task.contactsapi.controller;
 
+import hu.futureofmedia.task.contactsapi.model.dto.OutgoingContactPerson;
+import hu.futureofmedia.task.contactsapi.service.ContactPersonService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/contacts")
 @CrossOrigin(origins = "*")
 public class ContactPersonController {
 
+    @Autowired
+    private ContactPersonService contactPersonService;
+
     @GetMapping("/active")
-    public ResponseEntity<List<OutgoingContactPerson>> getAllActiveContactPersonsFromTo(@RequestParam("from") int from,
-                                                                                        @RequestParam("to") int to) {
+    public ResponseEntity<List<OutgoingContactPerson>> getAllActiveContactPersonsByPage(
+                                                        @RequestParam("page") int pageNumber) {
         try {
-            return ResponseEntity.ok(contactPersonService.getAllActiveContactPersonFromTo(from, to));
+            return ResponseEntity.ok(contactPersonService.getAllActiveContactPersonOnPage(pageNumber));
         } catch (Exception e) {
             throw new ResponseStatusException(
-                    HttpStatus.INTERNAL_SERVER_ERROR, "Error during getting active contacts from " + from + " - " + to);
+                    HttpStatus.INTERNAL_SERVER_ERROR, "Error during getting active contacts on page: " + pageNumber);
         }
     }
 
