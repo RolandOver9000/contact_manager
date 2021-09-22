de2e61325a326f2f742b91709be5d637bca01a41
Roland Csosz <roland@iot-inspector.com>
2021-09-22 10:39:51 +0200

    Added builder and data lombok annotations

--- src/main/java/hu/futureofmedia/task/contactsapi/model/dto/OutgoingContactPerson.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/model/dto/OutgoingContactPerson.java
@@ -1,5 +1,10 @@
 package hu.futureofmedia.task.contactsapi.model.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
 public class OutgoingContactPerson {
 
     private String fullName;
