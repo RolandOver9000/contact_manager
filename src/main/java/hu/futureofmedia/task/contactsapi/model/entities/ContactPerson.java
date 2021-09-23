5b0ec1d1c53d8a50911e68e6dab7e5fbf4b9c3c6
Roland Csosz <roland@iot-inspector.com>
2021-09-23 12:03:43 +0200

    Added lombok constructor related annotations and restrictions to fields

--- src/main/java/hu/futureofmedia/task/contactsapi/model/entities/ContactPerson.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/model/entities/ContactPerson.java
@@ -1,28 +1,47 @@
 package hu.futureofmedia.task.contactsapi.model.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @Entity
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ContactPerson {
 
     @Id
     @GeneratedValue
     private Long id;
+
+    @NotNull
+    @NotEmpty
     private String firstName;
+
+    @NotNull
+    @NotEmpty
     private String lastName;
 
     @Email
+    @NotNull
+    @NotEmpty
     private String email;
 
     private String phoneNumber;
 
     @OneToOne()
+    @NotNull
     private Company company;
+
+
     private String comment;
 
     @Enumerated(EnumType.STRING)
