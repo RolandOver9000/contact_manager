9b6ea228821addbefe714db42cf13c56abc669b6
Roland Csosz <roland@iot-inspector.com>
2021-09-23 12:07:39 +0200

    Changed the extended interface and added find company by name function

--- src/main/java/hu/futureofmedia/task/contactsapi/repositories/CompanyRepository.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/repositories/CompanyRepository.java
@@ -1,10 +1,12 @@
 package hu.futureofmedia.task.contactsapi.repositories;
 
 import hu.futureofmedia.task.contactsapi.model.entities.Company;
-import org.springframework.data.repository.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface CompanyRepository extends Repository<Company, Long> {
+public interface CompanyRepository extends CrudRepository<Company, Long> {
     List<Company> findAll();
+
+    Optional<Company> findByName(String name);
 }
