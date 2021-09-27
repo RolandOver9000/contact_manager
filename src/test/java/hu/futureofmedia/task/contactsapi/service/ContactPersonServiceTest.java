f948c78f547d4f6e52f17a5e221ae6a7f7ecbf86
Roland Csosz <roland@iot-inspector.com>
2021-09-27 14:34:18 +0200

    Added a test case for giving back 2 active contact persons

--- src/test/java/hu/futureofmedia/task/contactsapi/service/ContactPersonServiceTest.java
+++ src/test/java/hu/futureofmedia/task/contactsapi/service/ContactPersonServiceTest.java
@@ -47,4 +47,30 @@ public class ContactPersonServiceTest {
 
         Assertions.assertEquals(Collections.emptyList(), actualResultList);
     }
+
+    @Test
+    public void testGetAllActiveContactPersonAscendingByFirstNameByPageShouldReturnValidList() {
+        int pageNumber = 0;
+        int contactsPerPage = 10;
+        Pageable pageableObject = PageRequest.of(pageNumber,
+                contactsPerPage,
+                Sort.by("firstName").ascending());
+        List<ContactPerson> activeContactPersonList = new ArrayList<>(Arrays.asList(
+                contactPersonMother.getActiveContactPersonWithFistName("AAAAA"),
+                contactPersonMother.getActiveContactPersonWithFistName("BBBBB")
+        ));
+        Page<ContactPerson> contactPersonList = new PageImpl<>(activeContactPersonList);
+        List<OutgoingListedContactPersonDto> expectedResultList = activeContactPersonList
+                                .stream()
+                                .map(contactPersonMother::transformContactPersonToOutgoingListedContactPerson)
+                                .collect(Collectors.toList());
+
+        Mockito.when(contactPersonRepository
+                .findAllByStatus(Status.ACTIVE, pageableObject)).thenReturn(contactPersonList);
+        List<OutgoingListedContactPersonDto> actualResultList = contactPersonService
+                .getAllActiveContactPersonAscendingByFirstNameByPage(pageNumber);
+
+        Assertions.assertEquals(expectedResultList, actualResultList);
+        Assertions.assertEquals(2, actualResultList.size());
+    }
 }
